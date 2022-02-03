ba70063c3427a90469aabb024238430df2cc477a
cdmac94 <collinmacleod79@gmail.com>
2022-02-03 04:07:38 +0000

    Added dashboard with product and category count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,6 +4,6 @@ class Admin::DashboardController < ApplicationController
   def show
     @product_count = Product.count
  
-    @category_list = Product.group(:category_id).count
+    @category_list = Category.count
   end
 end
