d04e8fa660e86e1f0e1674431ba8807438f609da
cdmac94 <collinmacleod79@gmail.com>
2022-02-09 03:18:55 +0000

    Added spec for adding to cart

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
-
-  # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
 
@@ -10,9 +8,9 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
       @category.products.create!(
         name:  Faker::Hipster.sentence(3),
         description: Faker::Hipster.paragraph(4),
-        image: open_asset('apparel1.jpg'),
         quantity: 10,
-        price: 64.99
+        price: 64.99,
+        image: open_asset('apparel1.jpg')
       )
     end
   end
@@ -21,10 +19,9 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     # ACT
     visit root_path
 
-    # DEBUG
+    # DEBUG / VERIFY
     save_screenshot
-
-    # VERIFY
     expect(page).to have_css 'article.product', count: 10
   end
+
 end
